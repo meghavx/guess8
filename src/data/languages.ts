951dd9f143dc5d8d23bbba426e067cde7a77712f
Megha Verma <mverma1427@gmail.com>
2025-07-14 22:54:01 +0530

    Move data related files to a separate 'data' folder

---
+++ src/data/languages.ts
@@ -0,0 +1,47 @@
+export const languages = [
+  {
+    name: "JavaScript",
+    bg: "#2D519F",
+    color: "#F9F4DA",
+  },
+  {
+    name: "Python",
+    bg: "#328AF1",
+    color: "#F9F4DA",
+  },
+  {
+    name: "Ruby",
+    bg: "#F4EB13",
+    color: "#1E1E1E",
+  },
+  {
+    name: "Java",
+    bg: "#D02B2B",
+    color: "#F9F4DA",
+  },
+  {
+    name: "TypeScript",
+    bg: "#298EC6",
+    color: "#F9F4DA",
+  },
+  {
+    name: "Erlang",
+    bg: "#599137",
+    color: "#F9F4DA",
+  },
+  {
+    name: "Haskell",
+    bg: "#7A5EA7",
+    color: "#1E1E1E",
+  },
+  {
+    name: "Rust",
+    bg: "#E2680F",
+    color: "#F9F4DA",
+  },
+  {
+    name: "Assembly",
+    bg: "#2D519F",
+    color: "#F9F4DA",
+  },
+]
